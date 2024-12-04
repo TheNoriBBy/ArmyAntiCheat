55c8889d737169bee21c9003772ccbf8dfff774e
Patryk <151388537+TheNoriBBy@users.noreply.github.com>
2024-12-04 04:24:11 +0100

    Add files via upload

--- src/main/java/me/aviso/ArmyAntiCheat/Main.java
+++ src/main/java/me/aviso/ArmyAntiCheat/Main.java
@@ -9,6 +9,13 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import java.util.*;
 
 public class Main extends JavaPlugin {
@@ -26,6 +33,7 @@ public class Main extends JavaPlugin {
         instance = this;
         saveDefaultConfig();
         config = getConfig();
+        loadMovements();
     }
 
     @Override
@@ -33,6 +41,7 @@ public class Main extends JavaPlugin {
         playerMovements.clear();
         reportingPlayers.clear();
         playerMovementRecords.clear();
+        saveMovements();
     }
 
     public static Main getInstance() {
@@ -84,7 +93,56 @@ public class Main extends JavaPlugin {
                 sender.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("report-nonplayer", "&cTylko gracze moga uzywac tej komendy!")));
             }
             return true;
+        } else if (cmd.getName().equalsIgnoreCase("record")) {
+            if (sender instanceof Player) {
+                Player recorder = (Player) sender;
+                if (args.length != 1 || (!args[0].equalsIgnoreCase("real") && !args[0].equalsIgnoreCase("fake"))) {
+                    recorder.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("record-usage", "&cUzycie: &f/record <real|fake>")));
+                    return true;
+                }
+
+                String movementType = args[0].toLowerCase();
+                new PlayerMovementRecorder(recorder, movementType, 30).runTaskTimer(this, 0L, 1L);
+            } else {
+                sender.sendMessage(ChatColor.translateAlternateColorCodes('&', config.getString("record-nonplayer", "&cTylko gracze moga uzywac tej komendy!")));
+            }
+            return true;
         }
         return false;
+    }
+    private void loadMovements() {
+        loadMovementsFromFile(data.realmovements, "realmovements.RAAC");
+        loadMovementsFromFile(data.fakemovements, "fakemovements.RAAC");
+    }
+
+    private void loadMovementsFromFile(List<String> movements, String fileName) {
+        File file = new File(getDataFolder(), fileName);
+        if (file.exists()) {
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    movements.add(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void saveMovements() {
+        saveMovementsToFile(data.realmovements, "realmovements.RAAC");
+        saveMovementsToFile(data.fakemovements, "fakemovements.RAAC");
+    }
+
+    private void saveMovementsToFile(List<String> movements, String fileName) {
+        File file = new File(getDataFolder(), fileName);
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            for (String movement : movements) {
+                writer.write(movement);
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
